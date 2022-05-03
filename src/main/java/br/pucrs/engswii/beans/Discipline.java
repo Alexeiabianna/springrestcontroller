d6367b56d2bf54742ed2ba08feb969291ecf2085
ALEXEI MILITZ ABIANNA <ALEXEI.ABIANNA@edu.pucrs.br>
2022-05-03 18:37:43 -0300

    first commit registration discipline

---
+++ src/main/java/br/pucrs/engswii/beans/Discipline.java
@@ -0,0 +1,7 @@
+package br.pucrs.engswii.beans;
+
+public class Discipline {
+    String cod_disciplina;
+    String nome_disciplina;
+    String horario_disciplina;
+}
---
+++ src/main/java/br/pucrs/engswii/beans/DisciplineRegistration.java
@@ -0,0 +1,13 @@
+package br.pucrs.engswii.beans;
+
+import java.util.List;
+
+public class DisciplineRegistration {
+
+    private List<Discipline> disciplineRecords;
+
+    private static DisciplineRegistration discplineReg = null;
+
+    
+    
+}
