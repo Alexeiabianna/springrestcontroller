52272e5cf16b01318b72bef13a01ccae26fc6a3c
ALEXEI MILITZ ABIANNA <ALEXEI.ABIANNA@edu.pucrs.br>
2022-05-03 18:43:43 -0300

    add discipline registration

--- src/main/java/br/pucrs/engswii/beans/DisciplineRegistration.java
+++ src/main/java/br/pucrs/engswii/beans/DisciplineRegistration.java
@@ -1,5 +1,6 @@
 package br.pucrs.engswii.beans;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DisciplineRegistration {
@@ -8,6 +9,22 @@ public class DisciplineRegistration {
 
     private static DisciplineRegistration discplineReg = null;
 
-    
+    private DisciplineRegistration() {
+        disciplineRecords = new ArrayList<Discipline>();
+    }
+
+    public static DisciplineRegistration getInstance() {
+        if(discplineReg == null) {
+            discplineReg = new DisciplineRegistration();
+            return discplineReg;
+        }
+        else {
+            return discplineReg;
+        }
+    }
+
+    public void add(Discipline discipline) {
+        disciplineRecords.add(discipline);
+    }   
     
 }
