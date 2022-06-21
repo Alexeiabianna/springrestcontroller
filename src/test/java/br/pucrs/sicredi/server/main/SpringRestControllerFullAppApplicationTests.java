14b14344d01d3efa08819abc3ef33dfbbec89514
paulariett <paula.riett@edu.pucrs.br>
2022-06-21 18:01:57 -0300

    Adicionando arquivos e pastas de testes

--- src/test/java/br/pucrs/sicredi/server/main/SpringRestControllerFullAppApplicationTests.java
+++ src/test/java/br/pucrs/sicredi/server/main/SpringRestControllerFullAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringRestControllerFullAppApplicationTests {
+public class SpringRestControllerFullAppApplicationTests {
 
 	@Test
 	void contextLoads() {
